98e179d330127c9af22e08048ea7ae5aff4ebcff
ZivWu <zivwu@star-link.tech>
2025-01-07 15:57:28 +0800

    [Feat] WebError extend Error

--- src/modules/exception.js
+++ src/modules/exception.js
@@ -1,7 +1,8 @@
 const httpStatus  = require('http-status-codes');
 
-class WebError{
+class WebError extends Error{
     constructor(statusCode, code, message){
+        super(message)
         this.statusCode = statusCode;
         this.code = code;
         this.message = message;
