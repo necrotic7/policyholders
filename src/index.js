c52c48f21e3d779eb4a89955393b3e39e1339037
ZivWu <zivwu@star-link.tech>
2025-01-15 11:34:11 +0800

    [Feat] 新增graceful shutdown 與cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
 const database = require('./modules/database');
+const cors = require('cors');
 
 (async ()=>{
     // 初始化db
@@ -9,6 +10,7 @@ const database = require('./modules/database');
     // 初始化 Express 應用
     const app = express();
     app.use(bodyParser.json());
+    app.use(cors());
     app.use(controller);
 
     // 啟動伺服器
@@ -16,7 +18,14 @@ const database = require('./modules/database');
     app.listen(port, () => {
         console.log(`伺服器運行中，請訪問 http://localhost:${port}`);
     });
+
+    // graceful shutdown
+    process.on('SIGINT', async () => {
+        console.log('伺服器關閉中...');
+        await database.getDB.close()
+        process.exit(0);
+    });
 })().catch((err)=>{
-    console.log(err);
-    process.exit(0);
+    console.log('伺服器初始化失敗:', err);
+    process.exit(1);
 });
