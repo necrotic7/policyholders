1e20bee99d1fa797bb059ae31a4540e5fdbcb42b
ZivWu <zivwu@star-link.tech>
2025-02-10 14:58:40 +0800

    [Feat] 調整service return type

---
+++ src/model/service/PolicyHolders.ts
@@ -0,0 +1,10 @@
+export type PolicyData = {
+    code: string
+    name?: string
+    registration_date: string
+    introducer_code: string
+    level?: number
+    parent_id?: number
+    l: []
+    r: []
+};
