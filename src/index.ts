5acc1fb8bfd7f18a5ba2940cafc61efccd45d326
ZivWu <zivwu@star-link.tech>
2025-02-11 10:26:55 +0800

    [Feat] 加入koa搭配apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
 import database from 'workspace-modules/database';
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from '@apollo/server';
 import typeDefs from 'workspace-schema/policyHolders';
 import resolvers from 'workspace-resolvers/policyHolders';
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import { koaMiddleware } from '@as-integrations/koa';
+
 (async ()=>{
     // 初始化db
     await database.init();
     
     // 啟動apollo-server
-    const server: ApolloServer = new ApolloServer({
+    const server = new ApolloServer({
         typeDefs,
         resolvers,
     });
+    await server.start();
+    
+    // 初始化Koa
+    const app = new Koa();
+    app.use(bodyParser());
+    app.use(koaMiddleware(server));
 
-    const serverInfo = await server.listen();
-    console.log('server info:', JSON.stringify(serverInfo));
-
+    app.listen(3000, () => {
+        console.log('Server running on http://localhost:3000');
+    });
+    
     // graceful shutdown
     process.on('SIGINT', async () => {
         console.log('伺服器關閉中...');
