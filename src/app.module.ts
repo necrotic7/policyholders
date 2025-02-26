8df96066f4a2f6f38dff72eab32e00bb0d962e37
ZivWu <zivwu@star-link.tech>
2025-02-26 11:16:00 +0800

    [Feat] 新增db module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DirectiveLocation, GraphQLDirective } from 'graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // 載入 .env 設定
+    DatabaseModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
---
+++ src/database/database.module.ts
@@ -0,0 +1,9 @@
+import { Module, Global } from '@nestjs/common';
+import { DatabaseService } from './database.service';
+
+@Global() // 設定為全域模組，其他地方可以直接注入使用
+@Module({
+  providers: [DatabaseService],
+  exports: [DatabaseService],
+})
+export class DatabaseModule {}
