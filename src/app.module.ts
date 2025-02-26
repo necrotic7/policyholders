ec9a40cd97d591fa8e5e3713b412037b48d1ead3
ZivWu <zivwu@star-link.tech>
2025-02-26 11:15:12 +0800

    [Feat] 初始化apollo server

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      // 禁用playground
+      playground: false,
+      // 啟用sandbox
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+    }),
+  ],
+})
+export class AppModule {}
