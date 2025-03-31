62f197107545ef3a48745a2b29d725fc0956442d
ZivWu <ziv.wu@clinico.com.tw>
2025-03-31 16:03:11 +0800

    fix: 加上-vvv

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# mkdir -p ~/.ssh
+mkdir -p ~/.ssh
 # echo "$SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' > ~/.ssh/id_rsa
 # chmod 600 ~/.ssh/id_rsa
 
@@ -8,7 +8,7 @@ ssh-keyscan -H 35.209.10.171 >> ~/.ssh/known_hosts
 
 chmod 600 ./deploy/github_actions01
 echo "SSH Test"
-ssh -i ./deploy/github_actions01 -o "StrictHostKeyChecking=no" -tt github_actions_01@35.209.10.171
+ssh -vvv -i ./deploy/github_actions01 -o "StrictHostKeyChecking=no" -tt github_actions_01@35.209.10.171
 
 # echo "Deploying to ${SSH_USER}@${SSH_HOST}..."
 # # 把專案程式碼複製到遠端 VPS
