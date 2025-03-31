e926a892ef5d85977c7647e76425f1b9f0f8f2e3
ZivWu <ziv.wu@clinico.com.tw>
2025-03-31 15:16:36 +0800

    feat: 改成先測試ssh連線

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,13 +8,16 @@ echo "${SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -H ${SSH_HOST} >> ~/.ssh/known_hosts
 
-echo "Deploying to ${SSH_USER}@${SSH_HOST}..."
-# 把專案程式碼複製到遠端 VPS
-scp -r ./deploy/* ${SSH_USER}@${SSH_HOST}:/web/policyholders/
+echo "SSH Test"
+ssh -i ~/.ssh/id_rsa ${SSH_USER}@${SSH_HOST}
 
-if [ $? -ne 0 ]; then
-    echo "Error: SCP failed"
-    exit 1
-fi
+# echo "Deploying to ${SSH_USER}@${SSH_HOST}..."
+# # 把專案程式碼複製到遠端 VPS
+# scp -r ./deploy/* ${SSH_USER}@${SSH_HOST}:/web/policyholders/
 
-echo "Files copied successfully!"
+# if [ $? -ne 0 ]; then
+#     echo "Error: SCP failed"
+#     exit 1
+# fi
+
+# echo "Files copied successfully!"
