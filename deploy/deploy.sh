656c1c286ed8c607d1da32a6ff058b6d82b6b868
ZivWu <ziv.wu@clinico.com.tw>
2025-03-31 16:12:51 +0800

    feat: 調整debug

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,13 +6,13 @@ chmod 700 ~/.ssh
 # chmod 600 ~/.ssh/id_rsa
 cp ./deploy/github_actions01 ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-
+cat ~/.ssh/id_rsa
 ls -l ~/.ssh
 ssh-keyscan -H 35.209.10.171 >> ~/.ssh/known_hosts
 
 
 echo "SSH Test"
-ssh -vvv -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" -tt github_actions_01@35.209.10.171
+ssh -v -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" -tt github_actions_01@35.209.10.171
 
 # echo "Deploying to ${SSH_USER}@${SSH_HOST}..."
 # # 把專案程式碼複製到遠端 VPS
