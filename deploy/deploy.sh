be4a0d553f98c4d3985b87c3e21923baecda4de3
ZivWu <ziv.wu@clinico.com.tw>
2025-03-31 15:00:08 +0800

    feat: 調整ssh設定

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,15 @@
 # 讀取 GitHub Actions 傳進來的變數
+SSH_PRIVATE_KEY=${SSH_PRIVATE_KEY}
 SSH_USER=${SSH_USER}
 SSH_HOST=${SSH_HOST}
 
-echo "Deploying to ${SSH_USER}@${SSH_HOST}..."
+mkdir -p ~/.ssh
+echo "${SSH_PRIVATE_KEY}" | tr -d '\r' > ~/.ssh/id_rsa  # 修正換行問題
+chmod 600 ~/.ssh/id_rsa
+ssh-keyscan -H ${SSH_HOST} >> ~/.ssh/known_hosts
 
+echo "Deploying to ${SSH_USER}@${SSH_HOST}..."
+cat ~/.ssh/id_rsa
 # 把專案程式碼複製到遠端 VPS
 scp -r ./deploy/* ${SSH_USER}@${SSH_HOST}:/web/policyholders/
 
