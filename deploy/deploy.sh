27d39622135795fb1a641578e5b9526262ece373
ZivWu <ziv.wu@clinico.com.tw>
2025-04-01 17:13:48 +0800

    feat: 測試無passphrase的ssh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-# echo "$SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' > ~/.ssh/id_rsa
+# mkdir -p ~/.ssh
+# chmod 700 ~/.ssh
+# echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 # chmod 600 ~/.ssh/id_rsa
-cp ./deploy/github_actions01 ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa
+
 ls -l ~/.ssh
-ssh-keyscan -H 35.209.10.171 >> ~/.ssh/known_hosts
+# ssh-keyscan -H 35.209.10.171 >> ~/.ssh/known_hosts
 
+echo "Connect to ${SSH_USER}@${SSH_HOST}..."
+ssh -v -o "StrictHostKeyChecking=no" -tt ${SSH_USER}@${SSH_HOST}
 
-echo "SSH Test"
-ssh -v -o "StrictHostKeyChecking=no" -tt github_actions_01@35.209.10.171
-
-# echo "Deploying to ${SSH_USER}@${SSH_HOST}..."
 # # 把專案程式碼複製到遠端 VPS
 # scp -r ./deploy/* ${SSH_USER}@${SSH_HOST}:/web/policyholders/
 
