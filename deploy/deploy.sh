1e852f0f1ca9ed826bc1d100f2d921d8f358b63f
ZivWu <ziv.wu@clinico.com.tw>
2025-04-02 16:20:24 +0800

    feat: 移除測試指令 & 寫入完整gcp.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "Deploy to ${SSH_USER}@${SSH_HOST}..."
 SSH_CMD="ssh -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_HOST}"
-echo "actusr: ${DOCKER_USERNAME}"
-echo "actpwd: ${DOCKER_PASSWORD}"
 
 echo "DOCKER_USERNAME=${DOCKER_USERNAME}" > ./deploy/.env
 echo "DOCKER_PASSWORD=${DOCKER_PASSWORD}" >> ./deploy/.env
-cat ./deploy/.env
+
 # 把專案程式碼複製到遠端 VPS
 $SSH_CMD 'mkdir -p /web/policyholders/'
 scp -o "StrictHostKeyChecking=no" -r ./deploy ${SSH_USER}@${SSH_HOST}:/web/policyholders
@@ -19,7 +17,6 @@ fi
 # 遠端登入並執行 gcp.sh
 $SSH_CMD << 'EOF'
     cd /web/policyholders/deploy/
-    export TEST="123"
     chmod +x ./gcp.sh
     ./gcp.sh
 EOF
