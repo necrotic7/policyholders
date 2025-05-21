c1920bcf3ba9989c9d7b8d5f7b31edcd59893f97
ZivWu <ziv.wu@clinico.com.tw>
2025-05-21 11:20:21 +0800

    feat: gcp.sh增加刪除同名container指令

--- deploy/remote/gcp.sh
+++ deploy/remote/gcp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source ../.env
 
+docker rm -f policyholders
+
 # 使用 docker-compose 啟動 postgresql & Policyholders 容器
 echo "Starting Policyholders container..."
 docker compose  --env-file ../.env up -d
