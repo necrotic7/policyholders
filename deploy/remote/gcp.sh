bfed1526e4f7bf2cadae40e8ed5eecf589fcb4ab
ZivWu <ziv.wu@clinico.com.tw>
2025-05-21 11:04:20 +0800

    fix: 調整gcp.sh

--- deploy/remote/gcp.sh
+++ deploy/remote/gcp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source deploy/.env
+source ../.env
 
 # 使用 docker-compose 啟動 postgresql & Policyholders 容器
 echo "Starting Policyholders container..."
-docker-compose -f deploy/remote/docker-compose.yml --env-file deploy/.env up -d
+docker compose  --env-file ../.env up -d
 if [ $? -ne 0 ]; then
     echo "Error: Failed to docker compose"
     exit 1  # 停止腳本執行
