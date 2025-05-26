45a40838f180049330af84a0f01df08ba18dcbd4
ZivWu <ziv.wu@clinico.com.tw>
2025-05-26 14:32:59 +0800

    feat: 修改複製到遠端的路徑

--- deploy/remote/deploy.sh
+++ deploy/remote/deploy.sh
@@ -50,7 +50,8 @@ SSH_CMD="ssh -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_HOST}"
 # 把專案程式碼複製到遠端 VPS
 $SSH_CMD 'rm -rf /web/policyholders'
 $SSH_CMD 'mkdir -p /web/policyholders/'
-scp -o "StrictHostKeyChecking=no" -r ./deploy ${SSH_USER}@${SSH_HOST}:/web/policyholders
+scp -o "StrictHostKeyChecking=no" -r ./deploy/remote/* ${SSH_USER}@${SSH_HOST}:/web/policyholders
+scp -o "StrictHostKeyChecking=no" ./deploy/.env ${SSH_USER}@${SSH_HOST}:/web/policyholders
 
 if [ $? -ne 0 ]; then
     echo "Error: SCP failed"
@@ -59,7 +60,7 @@ fi
 
 # 遠端登入並執行 gcp.sh
 $SSH_CMD << 'EOF'
-    cd /web/policyholders/deploy/remote
+    cd /web/policyholders
     chmod +x ./gcp.sh
     ./gcp.sh
 EOF
--- deploy/remote/gcp.sh
+++ deploy/remote/gcp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-source ../.env
+source .env
 
 docker rm -f policyholders
 
 # 使用 docker-compose 啟動 postgresql & Policyholders 容器
 echo "Starting Policyholders container..."
-docker compose  --env-file ../.env up -d
+docker compose --env-file .env up -d
 if [ $? -ne 0 ]; then
     echo "Error: Failed to docker compose"
     exit 1  # 停止腳本執行
