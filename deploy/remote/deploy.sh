d39fa15a78251d410ae3a42ab08d2cde8d65a792
ZivWu <ziv.wu@clinico.com.tw>
2025-05-27 14:34:33 +0800

    feat: 測試npm 打版

--- deploy/remote/deploy.sh
+++ deploy/remote/deploy.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 source deploy/.env
 
-# 1. 更新版號
+# 1. GitHub Actions bot 設定身份
+git config --global user.name "github-actions[bot]"
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+
+# 改 remote，用 GITHUB_TOKEN 認證
+git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+# 2. 更新版號
 echo "Start to update versions"
 LAST_VERSION=$(npm pkg get version | jq -r .)
 VERSION=$(npm version patch)
 echo "update version from($LAST_VERSION) to($VERSION)"
 # 將新版號寫入env
 echo "VERSION=$VERSION" >> deploy/.env
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to update version"
+    exit 1
+fi
+
 
 # 3. build app
 echo "Building Policyholders Docker image..."
@@ -19,10 +32,15 @@ fi
 
 # 4. push to docker hub
 echo "Pushing to Docker Hub"
-echo "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
+echo "docker login $DOCKER_USERNAME"
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker tag policyholders:$VERSION $DOCKER_USERNAME/policyholders:$VERSION
 docker push $DOCKER_USERNAME/policyholders:$VERSION
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to push to docker hub"
+    exit 1  # 停止腳本執行
+fi
 
 # 5. 部署到遠端機器 
 echo "Deploy to ${SSH_USER}@${SSH_HOST}..."
